7284662ca6388a876b5d506162ab980e8748c600
Marina Alane <marina.alane@outlook.com>
2021-12-10 17:30:50 -0300

    exercicios com operadores aritmeticos

---
+++ Arrays/Arrays.rb
@@ -0,0 +1,12 @@
+É possível declarar de duas formas diferentes :
+
+m = [12,25,36,96,35,28,15]
+puts m
+end
+
+v = Array.new
+v.push(20)
+v.push(32)
+
+puts v
+end
---
+++ EstruturasRepeticao/each.rb
@@ -0,0 +1,9 @@
+# Each
+
+['A','b',3].each do |item|
+  puts "O valor lido foi: " + item.to_s
+end
+
+(1...5).each do |item|
+  puts "O i é o: " + item.to_s
+end
